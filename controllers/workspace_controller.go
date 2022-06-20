c1868bb169ebb52e047d93c3d88995c614b7701c
Aleksandr Rybolovlev <asr@netcow.org>
2022-06-20 11:11:08 +0200

    Add initial reconcile logic.

--- controllers/workspace_controller.go
+++ controllers/workspace_controller.go
@@ -18,13 +18,21 @@ package controllers
 
 import (
 	"context"
+	"time"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	appv1alpha2 "github.com/arybolovlev/terraform-cloud-kubernetes-operator/api/v1alpha2"
+)
+
+const (
+	requeueInterval    = 60 * time.Second
+	workspaceFinalizer = "workspace.app.terraform.io/finalizer"
 )
 
 // WorkspaceReconciler reconciles a Workspace object
@@ -39,17 +47,22 @@ type WorkspaceReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Workspace object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.1/pkg/reconcile
 func (r *WorkspaceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	instance := &appv1alpha2.Workspace{}
+	err := r.Client.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
+		// 'Not found' error occurs when an object is removed from the Kubernetes
+		// No actions are required at this state
+		if errors.IsNotFound(err) {
+			return r.doNotRequeue()
+		}
+		return r.requeueAfter(requeueInterval)
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -59,4 +72,16 @@ func (r *WorkspaceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&appv1alpha2.Workspace{}).
 		Complete(r)
+}
+
+func (r *WorkspaceReconciler) doNotRequeue() (reconcile.Result, error) {
+	return reconcile.Result{}, nil
+}
+
+func (r *WorkspaceReconciler) requeueOnErr(err error) (reconcile.Result, error) {
+	return reconcile.Result{}, err
+}
+
+func (r *WorkspaceReconciler) requeueAfter(duration time.Duration) (reconcile.Result, error) {
+	return reconcile.Result{Requeue: true, RequeueAfter: duration}, nil
 }
